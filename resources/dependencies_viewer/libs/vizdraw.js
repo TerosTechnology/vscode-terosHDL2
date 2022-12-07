e22674755eac9e29545e369872ce49de7fdc2b5e
qarlosalberto <carlosruiznaranjo@gmail.com>
2022-12-07 10:49:05 +0100

    New init

---
+++ resources/dependencies_viewer/libs/vizdraw.js
@@ -0,0 +1,63 @@
+"use strict";
+// default
+const vscode = acquireVsCodeApi();
+
+document.getElementById("export-as-image").onclick = function() {export_message("image")};
+
+function export_message(message) {
+    vscode.postMessage({
+        command: 'export',
+        text: message
+    });
+}
+
+// Handle the message inside the webview
+window.addEventListener('message', event => {
+    const message = event.data; // The JSON data our extension sen
+    switch (message.command) {
+        case 'update':
+            update_graph(message.message);
+            break;
+        case 'clear':
+            clear();
+            break;
+    }
+});
+
+var graph = null;
+function update_graph(dot) {
+    let viz = new Viz();
+    viz.renderSVGElement(dot)
+        .then(function (element) {
+            if (graph !== null) {
+                document.body.removeChild(graph);
+            }
+            document.body.appendChild(element);
+            graph = element;
+
+            panZoom = svgPanZoom(element, panConfig);
+            panZoom.zoom(0.8);
+            panZoom.center();
+            panZoom.resize();
+            trimZoomBtns();
+        });
+}
+function clear() {
+    if (graph !== null) {
+        document.body.removeChild(graph);
+        graph = null;
+    }
+}
+function trimZoomBtns() {
+    $('#svg-pan-zoom-zoom-in rect.svg-pan-zoom-control-background').attr('rx', 300);
+    $('#svg-pan-zoom-zoom-out rect.svg-pan-zoom-control-background').attr('rx', 300);
+    $('#svg-pan-zoom-reset-pan-zoom rect.svg-pan-zoom-control-background').attr('rx', 13);
+}
+let panConfig = {
+    zoomEnabled: true,
+    controlIconsEnabled: true,
+    fit: true,
+    center: true,
+};
+let panZoom;
+
